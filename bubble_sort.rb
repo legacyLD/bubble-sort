200adf45b7d0392b42f8de049d06d1bf22eec9e6
vedelopment <vedelopment@gmail.com>
2017-02-02 18:26:14 -0800

    progress

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,3 +1,16 @@
 def bubble_sort(arr)
-  # code in here
+  swapped = true
+  length = arr.length
+  count = 0
+
+  while condition
+    if condition
+      count > count + 1
+    end
+    else
+      swapped = false
+    end
+    count + 1
+  end
+
 end
